83514e7f13b92b5f0172dfa190bca23fc3208473
joao.mgcruz <joao.mgcruz@gmail.com>
2013-06-26 20:00:21 +0000

    Modificação 1

--- Calculadora/JavaSource/br/ufrn/ui/OperacaoBean.java
+++ Calculadora/JavaSource/br/ufrn/ui/OperacaoBean.java
@@ -27,7 +27,14 @@ public class OperacaoBean {
 	
 	public String operacaoAdicao(){
 		setAlerta("");
-		Operate(new OperacaoAdicao());
+		OperacaoAdicao o = new OperacaoAdicao();
+		try{
+			o.setTermoUm(Float.valueOf(termoUm));
+			o.setTermoDois(Float.valueOf(termoDois));
+			resultado = o.resultado().toString();
+		}catch(NumberFormatException nfe){
+			alerta = "Os valores dos termos só podem receber números.";
+		}
 		return "index";
 	}
 	
