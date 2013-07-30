1eaac727285afa2b33209a43cad81f468920b8d1
felipeapp <felipeapp@gmail.com>
2013-07-30 21:21:37 +0000

    Adicionada classe de consulta ao banco de dados do iproject.

---
+++ br.ufrn.ppgsc.scenario.analyzer.backhoe/src/br/ufrn/ppgsc/scenario/analyzer/backhoe/AnalyzerMiner.java
@@ -0,0 +1,74 @@
+package br.ufrn.ppgsc.scenario.analyzer.backhoe;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.tmatesoft.svn.core.SVNException;
+
+import br.ufrn.backhoe.repminer.connector.Connector;
+import br.ufrn.backhoe.repminer.connector.SubversionConnector;
+import br.ufrn.backhoe.repminer.enums.ConnectorType;
+import br.ufrn.backhoe.repminer.factory.connector.SubversionConnectorFactory;
+
+public class AnalyzerMiner {
+
+	public static Collection<UpdatedMethod> getUpdatedMethodsFromRepository(String host,
+			String user, String password, String file_path, String old_worcopy, String new_workcopy) {
+
+		long old_revision = 0, new_revision = 0;
+		
+		SubversionConnectorFactory svnFactory = new SubversionConnectorFactory();
+		SubversionConnector svnConnector = svnFactory.createConnector(user, password, host);
+
+		Map<ConnectorType, Connector> connectors = new HashMap<ConnectorType, Connector>();
+		connectors.put(ConnectorType.SVN, svnConnector);
+		
+		try {
+			old_revision = ChangedAssetsMinerUtil.getCommittedRevisionNumber(old_worcopy);
+			new_revision = ChangedAssetsMinerUtil.getCommittedRevisionNumber(new_workcopy);
+		} catch (SVNException e) {
+			e.printStackTrace();
+		}
+
+		Map parameters = new HashMap<String, String>();
+		parameters.put("target_path", file_path);
+		parameters.put("start_revision", old_revision);
+		parameters.put("end_revision", new_revision);
+
+		RevisionOfChangedAssetsMinerNoDB miner = new RevisionOfChangedAssetsMinerNoDB();
+		miner.setConnectors(connectors);
+		miner.setParameters(parameters);
+
+		miner.performSetup();
+		miner.performMining();
+
+		return (Collection<UpdatedMethod>) parameters.get("result");
+		
+	}
+	
+	public static Collection<UpdatedMethod> getUpdatedMethodsFromRepository(String host,
+			String user, String password, String file_path, long old_revision, long new_revision) {
+		
+		SubversionConnectorFactory svnFactory = new SubversionConnectorFactory();
+		SubversionConnector svnConnector = svnFactory.createConnector(user, password, host);
+
+		Map<ConnectorType, Connector> connectors = new HashMap<ConnectorType, Connector>();
+		connectors.put(ConnectorType.SVN, svnConnector);
+
+		Map parameters = new HashMap<String, String>();
+		parameters.put("start_revision", old_revision);
+		parameters.put("end_revision", new_revision);
+
+		RevisionOfChangedAssetsMinerNoDB miner = new RevisionOfChangedAssetsMinerNoDB();
+		miner.setConnectors(connectors);
+		miner.setParameters(parameters);
+
+		miner.performSetup();
+		miner.performMining();
+
+		return (Collection<UpdatedMethod>) parameters.get("result");
+		
+	}
+
+}
--- br.ufrn.ppgsc.scenario.analyzer.tests/src/miner/TestMiner.java
+++ br.ufrn.ppgsc.scenario.analyzer.tests/src/miner/TestMiner.java
@@ -1,15 +1,8 @@
 package miner;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
-import br.ufrn.backhoe.repminer.connector.Connector;
-import br.ufrn.backhoe.repminer.connector.SubversionConnector;
-import br.ufrn.backhoe.repminer.enums.ConnectorType;
-import br.ufrn.backhoe.repminer.factory.connector.SubversionConnectorFactory;
-import br.ufrn.ppgsc.scenario.analyzer.backhoe.ChangedAssetsMinerUtil;
-import br.ufrn.ppgsc.scenario.analyzer.backhoe.RevisionOfChangedAssetsMinerNoDB;
+import br.ufrn.ppgsc.scenario.analyzer.backhoe.AnalyzerMiner;
 import br.ufrn.ppgsc.scenario.analyzer.backhoe.UpdatedLine;
 import br.ufrn.ppgsc.scenario.analyzer.backhoe.UpdatedMethod;
 
@@ -17,36 +10,12 @@ public class TestMiner {
 	
 	public static void main(String[] args) throws Exception {
 		
-		String user = "";
-		String password = "";
-		String svnhost = "http://scenario-analyzer.googlecode.com/svn";
-
-		SubversionConnectorFactory svnFactory = new SubversionConnectorFactory();
-		SubversionConnector svnConnector = svnFactory.createConnector(user, password, svnhost);
-
-		Map<ConnectorType, Connector> connectors = new HashMap<ConnectorType, Connector>();
-		connectors.put(ConnectorType.SVN, svnConnector);
-
-		long rold = ChangedAssetsMinerUtil.getCommittedRevisionNumber(
-				"C:/Eclipse/JunoDoutorado/workspace/br.ufrn.ppgsc.scenario.analyzer.tests_oldrevision/src/tests/Main.java");
-
-		long rnew = ChangedAssetsMinerUtil.getCommittedRevisionNumber(
-				"C:/Eclipse/JunoDoutorado/workspace/br.ufrn.ppgsc.scenario.analyzer.tests/src/tests/Main.java");
-
-		Map parameters = new HashMap<String, String>();
-		parameters.put("target_path", "/trunk/br.ufrn.ppgsc.scenario.analyzer.tests/src/tests/Main.java");
-		parameters.put("start_revision", rold);
-		parameters.put("end_revision", rnew);
-
-		RevisionOfChangedAssetsMinerNoDB miner = new RevisionOfChangedAssetsMinerNoDB();
-		miner.setConnectors(connectors);
-		miner.setParameters(parameters);
-
-		miner.performSetup();
-		miner.performMining();
-
-		Collection<UpdatedMethod> result = (Collection<UpdatedMethod>) parameters.get("result");
-
+		Collection<UpdatedMethod> result = AnalyzerMiner.getUpdatedMethodsFromRepository(
+				"http://scenario-analyzer.googlecode.com/svn", "", "",
+				"/trunk/br.ufrn.ppgsc.scenario.analyzer.tests/src/tests/Main.java",
+				"C:/Eclipse/EclipseDoutorado/workspace/br.ufrn.ppgsc.scenario.analyzer.tests_oldrevision/src/tests/Main.java",
+				"C:/Eclipse/EclipseDoutorado/workspace/br.ufrn.ppgsc.scenario.analyzer.tests/src/tests/Main.java");
+		
 		for (UpdatedMethod m : result) {
 			System.out.println("******************************************");
 			System.out.println(m.getMethodLimit().getSignature());
@@ -60,6 +29,42 @@ public class TestMiner {
 				System.out.println("\t-------------------------------------");
 			}
 		}
+		
+//		String text = "Revisão: 4980 por felipe.";
+//		
+//		String re5 = "(\\s)";
+//		String re6 = "(\\d+)";
+//		String re7 = "(:)";
+//		
+//		Pattern p2 = Pattern.compile(re5 + re6 + re5, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+//		Pattern p3 = Pattern.compile(re7 + re6 + re5);
+//		Pattern p6 = Pattern.compile(re5 + re6);
+//		
+//		long revision = -1;
+//		
+//		for (String parts : text.split("Revisão")) {
+//			System.out.println(parts);
+//		
+//			Matcher revMatcher = p2.matcher(parts);
+//			
+//			// (begin) get the revision
+//			if (revMatcher.find()) {
+//				revision = Long.valueOf(revMatcher.group(2));
+//			} else {
+//				revMatcher = p3.matcher(parts);
+//				if (revMatcher.find()) {
+//					revision = Long.valueOf(revMatcher.group(2));
+//				} else {
+//					revMatcher = p6.matcher(parts);
+//					if (revMatcher.find()) {
+//						revision = Long.valueOf(revMatcher.group(2));
+//					}
+//				}
+//			}
+//			// (end)
+//		}
+//		
+//		System.out.println("Revisão: " + revision);
 
 	}
 	
