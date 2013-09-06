9030717d60f67bdafb5e959f5d48ae007282124d
joao.mgcruz <joao.mgcruz@gmail.com>
2013-09-06 14:57:06 +0000

    Modificação 1

--- Exemplo/src/exemplo/Classe1_2.java
+++ Exemplo/src/exemplo/Classe1_2.java
@@ -7,8 +7,6 @@ public class Classe1_2 {
 	
 	@Test
 	public void chamaMethodClasse2_2() {
-		Classe2_2 c2_2 = new Classe2_2();
-		c2_2.chamarMetodoClasse3();
 		Classe2_3 c2_3 = new Classe2_3();
 		c2_3.chamarMetodoClasse3();
 		Assert.assertTrue(true);
