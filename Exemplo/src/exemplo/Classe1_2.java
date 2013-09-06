9bbbd024e255a03aa22c2b48371f6860d3fc86f0
joao.mgcruz <joao.mgcruz@gmail.com>
2013-09-06 14:49:21 +0000

    Início

--- Exemplo/src/exemplo/Classe1_2.java
+++ Exemplo/src/exemplo/Classe1_2.java
@@ -9,6 +9,8 @@ public class Classe1_2 {
 	public void chamaMethodClasse2_2() {
 		Classe2_2 c2_2 = new Classe2_2();
 		c2_2.chamarMetodoClasse3();
+		Classe2_3 c2_3 = new Classe2_3();
+		c2_3.chamarMetodoClasse3();
 		Assert.assertTrue(true);
 	}
 
--- Exemplo/src/exemplo/Classe2_3.java
+++ Exemplo/src/exemplo/Classe2_3.java
@@ -2,6 +2,9 @@ package exemplo;
 
 public class Classe2_3 {
 
-	
+	public void chamarMetodoClasse3() {
+		Classe3_2 c3_2 = new Classe3_2();
+		c3_2.chamarMetodoClasse4();
+	}
 	
 }
